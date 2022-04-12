c78d45e7e269d49a843ce77957c6c0d682b3f5a0
NikolayLazarov <48582131+NikolayLazarov@users.noreply.github.com>
2022-04-13 00:54:01 +0300

    Update object.cpp
    Put hardcoded functions for brick borders and no overwrapping of object and wall

--- Main_code_v2/object.cpp
+++ Main_code_v2/object.cpp
@@ -102,37 +102,89 @@ Object::Object(){
 //    
 //  }
 
+//void compare_cords(int ){
+  
+//}
+
+    bool Object::check_borders(/*Object & object,*/ int object_x, int object_y_up, int object_y_down){
+        bool flag = true;
+        if (object_x >= 175 && object_x <=277){
+            if (object_y_up >=32 &&object_y_up<=32+30){
+            flag = false;  
+            }
+            //obj_y_1 down obj_y_2 up
+            else if ((object_y_down >=128 &&object_y_up<=160+30)/* || ( object_y_2 >=128 &&object_y_2<=160+30 )*/){
+              flag = false;
+            }
+            else if (object_y_down >=256 &&object_y_down<=256+30){
+              flag = false;
+            }
+        }
+        return flag;
+        
+    }
+
+    bool Object::borders_Y(/*Object & object,*/ int object_x_r, int object_x_l, int object_y){
+      bool flag = true;
+      if (object_x_r >= 175 && object_x_l <=277+30) {
+        if (object_y >=32 &&object_y<=32+30){
+          flag = false;
+        }
+        else if (object_y >=128 &&object_y<=160+30){
+          flag = false;
+        }
+        else if (object_y >=256 &&object_y<=256+30){
+          flag = false;
+        }
+      }
+      return flag;
+    }
+
    void Object::hero_move_right(){
+    int object_x = this->x+ this->width_hero + 15;
+    int object_y_up = this->y;
+    int object_y_down = this->y+this->width_hero+15;
     
-    if (!(this->x+ this->width_hero + 15 >= WIDTH-30)){
+    if (object_x >= WIDTH-30){
+      Serial.println("Hitting right");
+    }
+    else if (check_borders(object_x, object_y_up,object_y_down) ==  false)
+    {
+      Serial.println("Hitting forbiden right spaces");
+    }
+    else{
       delete_hero();
     delete_weapon();
     set_x(this->x+5);
    make_hero();
      make_weapon_right();
     }
-    else{
-      Serial.println("Hitting right");
-    }
 
 //this->weapon_x+15 < 480-30?
     
   }
    void Object::hero_move_left(){
-    Serial.print("weapon_x = ");
-    Serial.println(weapon_x);
+    int object_x = this->x-10-5-30;
+//    int object_x_r = this->x
+//     int object_x = this->x -(20); 
+     int object_y_up = this->y;
+     int object_y_down = this->y+this->width_hero;
     //depends on size of object
-    if (!(this->x -(20) < 30 )){
+    if (object_x < 30 ){
+    Serial.println("Hitting left");
+    }
+    else if (check_borders(object_x, object_y_up,object_y_down) ==  false){
+       Serial.println("Hitting forbidden left");    
+    }
+    else{
       delete_hero();
-  delete_weapon();
+    delete_weapon();
 //    delay(100);
     set_x(this->x-5);
     make_hero();
     make_weapon_left();
 //  delete_weapon(this->weapon_x, this->weapon_y);
-    }
-    else{
-      Serial.println("Hitting left");
+      
     }
 
     
@@ -140,37 +192,47 @@ Object::Object(){
     
   }
   void Object::hero_move_up(){
-if (this->y-20 >= 30 /*&& this->weapon_y > 30 */ ){
+    int object_x_l = this->x;
+    int object_x_r = this->x+ this->width_hero;//not sure
+    int object_y = this->y-17;
+//int object_y = this->y;
+if (object_y <= 30 /*&& this->weapon_y > 30 */ ){
+  Serial.println("Hitting up");
+  } 
+  else if(borders_Y(object_x_r, object_x_l, object_y)== false)
+    {
+      Serial.println("Hitting forbidden up");
+    }
+ else{
     delete_hero();
     delete_weapon();
-//    delay(100);
     set_y(this->y-5);
     make_hero();
     make_weapon_up();
-//  delete_weapon(this->weapon_x, this->weapon_y);
- 
+ }
   
 }
     
-    
-  }
   void Object::hero_move_down(){
-      
-    if (!(this->y+ this->height_hero + 20 > HEIGHT-30)){
-         delete_hero();
+   int object_x_l = this->x;//not sure
+      int object_x_r = this->x+ this->width_hero;//not sure
+    int object_y = this->y+ this->height_hero + 15;
+    if ( object_y > HEIGHT-30){
+         Serial.println("Hitting up");
+    }
+   else if (borders_Y(object_x_r, object_x_l, object_y)== false)
+    {
+      Serial.println("Hitting down");
+    }
+    else{
+    delete_hero();
     delete_weapon();
-//    delay(100);
     set_y(this->y+5);
     make_hero();
     make_weapon_down();
-//  delete_weapon(this->weapon_x, this->weapon_y);
-      
+ 
     }
-    
-//    if (this->y+this->height_hero < 320-30 &&  this->weapon_y+15 < 320-30  ){ }
   }
-//
-
 
   void Object::make_weapon_right(){
     //set_weapon_x(width  +/-);
@@ -202,11 +264,12 @@ if (this->y-20 >= 30 /*&& this->weapon_y > 30 */ ){
       tft.fillRect(this->weapon_x,this->weapon_y,10,10,BLACK);
   }
 
-    void Object::bullet_right(Object &other){
+    bool Object::bullet_right(Object &other){
+      bool flag = true;
      char dir = this->direct;
      Serial.print("this is the direction ");
      Serial.print(dir);
-//
+//  
       switch(dir){
         case 1:
         Serial.println("this is right");
@@ -219,8 +282,12 @@ if (this->y-20 >= 30 /*&& this->weapon_y > 30 */ ){
         {        
           //fix issue where it doesnt allays puts purple and overrides the shots
           tft.fillRect(next_bullet,this->weapon_y, 30,5, PURPLE);
-//          restart();
+          delay(500);
+          tft.fillRect(next_bullet,this->weapon_y, 30,5, BLACK);
+          other.set_lives(other.get_lives()-1);
+          flag = false; 
           break;
+  
           }
         else{
           tft.fillRect(next_bullet,this->weapon_y, 30,5, OLIVE);
@@ -242,6 +309,10 @@ if (this->y-20 >= 30 /*&& this->weapon_y > 30 */ ){
            && (//checks whether the enemy is near the same y cords 
             this->weapon_y >= other.get_y()-5 && this->weapon_y <=other.get_y()+20)){
             tft.fillRect(next_bullet ,this->weapon_y, 30,5, PURPLE);
+            delay(1000);
+            tft.fillRect(next_bullet ,this->weapon_y, 30,5, BLACK);
+            other.set_lives(other.get_lives()-1);
+            flag = false;
             break;          
           }
           else{
@@ -262,6 +333,10 @@ if (this->y-20 >= 30 /*&& this->weapon_y > 30 */ ){
             (next_bullet+30 >= other.get_y() &&next_bullet + 30 <= other.get_y()+20))&&
           (this->weapon_x>=other.get_x()-5 && this->weapon_x <=other.get_x()+20)){
             tft.fillRect(this->weapon_x,next_bullet, 5,30, PURPLE);
+            delay(1000);
+            tft.fillRect(this->weapon_x,next_bullet, 5,30, BLACK);
+            other.set_lives(other.get_lives()-1);
+            flag = false;
             break;
           }
           else{
@@ -273,26 +348,31 @@ if (this->y-20 >= 30 /*&& this->weapon_y > 30 */ ){
         break;
         
         case 4:Serial.println("this is down"); 
-        for (int i =  0; i <5; i ++){
+        for (int i =  0; i <4; i ++){
           int next_bullet = this->weapon_y + 10  + (i * 30);
           if ((next_bullet >= other.get_y())&&
             ((next_bullet+30 >= other.get_y() &&next_bullet+30 <= other.get_y()+20)||//sth is not right here
             (next_bullet >= other.get_y() && next_bullet <= other.get_y()+20))
             &&(this->weapon_x >= other.get_x()-5 && this->weapon_x <= other.get_x() +20)){
               tft.fillRect(this->weapon_x,next_bullet, 5,30, PURPLE);
+              delay(100);
+              tft.fillRect(next_bullet,this->weapon_y, 30,5, BLACK);
+              other.set_lives(other.get_lives()-1);
+              
+              flag = false;
               break;
             }
           
 //        tft.fillRect(this->weapon_x,this->weapon_y+ 10 + (i * 30), 30,5, OLIVE);
         tft.fillRect(this->weapon_x,next_bullet, 5,30, OLIVE);
         delay(100);
-//        tft.fillRect(this->x,this->weapon_y + 10 + (i * 30), 30,5, BLACK);
+        
       tft.fillRect(this->weapon_x,next_bullet, 5,30, BLACK);
       }    break;
         
         default:Serial.println("none");break;
       }
       
-      
+      return flag;
     }
   
